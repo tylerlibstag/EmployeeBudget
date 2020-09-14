378bdeee1eb30126408eceba18fe15bb1d9163a4
TYTY <tylerlibstag@gmail.com>
2020-09-14 15:33:50 -0400

    fixed naming convention

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/EmployeeDirectory", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/EmployeeBudget", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
